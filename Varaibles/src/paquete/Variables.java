dc6378fdf1c2d559fba86db4b70235f583cad2ed
AgusBaez <agustinbaezignacio@gmail.com>
2021-01-16 15:08:59 -0300

    Variables

---
+++ Varaibles/src/paquete/Variables.java
@@ -0,0 +1,40 @@
+package paquete;
+
+class Varaibles {
+    public static void main (String[] args) {
+        //anotaciones
+        int Num_1;
+        Num_1 = 5;
+        System.out.println("Este va a ser mi numero con la varaible int"+Num_1);
+        int resultado;
+        //Numeros enteros Varaibles: Short,int,long
+        resultado = Num_1 - 2;
+        System.out.println("Este es mi numero Restado por 2 : "+resultado);
+        resultado = Num_1 + 2;
+        System.out.println("Este es mi numero Mas 2 : "+resultado);
+        resultado = Num_1 / 2;
+        System.out.println("Este es mi numero Dividido en 2 : "+resultado);
+        resultado = Num_1 * 2;
+        System.out.println("El numero Multiplicado por 2 es igual a : "+resultado);
+        resultado = Num_1 % 2;
+        System.out.println("El numero Dividido por 2 es igual a : "+resultado);
+        System.out.println(" ");
+        //numeros Decimales Varaibles float,double
+        float Num_2;
+        Num_2 = 5;
+        System.out.println("Este va a ser mi numero con la varaible float "+Num_2);
+        float resultado2;
+        resultado2 = Num_2 - 2;
+        System.out.println("Este es mi numero Restado por 2 : "+resultado2);
+        resultado2 = Num_2 + 2;
+        System.out.println("Este es mi numero Mas 2 : "+resultado2);
+        resultado2 = Num_2 / 2;
+        System.out.println("Este es mi numero Dividido en 2 : "+resultado2);
+        resultado2 = Num_2 * 2;
+        System.out.println("El numero Multiplicado por 2 es igual a : "+resultado2);
+        resultado2 = Num_2 % 2;
+        System.out.println("El numero Dividido por 2 es igual a : "+resultado2);
+        //Varibles no incluidas Byte,Booleanas y  las char.
+
+    }
+}
