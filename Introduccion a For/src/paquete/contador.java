285658d160b7976c323822643ca3eb2b5d500f58
AgusBaez <agustinbaezignacio@gmail.com>
2021-01-20 21:06:16 -0300

    Primeros pasos For+

---
+++ Introduccion a For/src/paquete/contador.java
@@ -0,0 +1,9 @@
+package paquete;
+class Main {
+    public static void main(String[] args){
+        for (int contador = 0;contador <=10;contador++){
+            System.out.println("El contador vale: "+contador);
+        }
+
+    }
+}
